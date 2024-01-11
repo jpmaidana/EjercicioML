b0a09645831bed307d140945b081f2b3c04f07dc
Jean Paul Maidana <39135549+jpmaidana@users.noreply.github.com>
2024-01-11 20:26:11 -0300

    Add files via upload

---
+++ Maquinas.R
@@ -0,0 +1,14 @@
+#load libraries
+library(tidyverse) #análisis exploratorio
+library(dlookr) #resumen medidas estadísticas
+library(visdat) #tipos de variables
+library(inspectdf) #gráfico cdf
+library(qqplotr) #grafigo Q-Q
+library(funModeling) #freq
+library(flextable) #imprimir tablas
+library(outliers) #análisis de atípicos
+library(plotly) #gráfico dinámico
+library(VIM) #datos faltantes
+library(mice) #imputación datos faltantes
+library(samplingbook) #estimat tamaño de muestra
+
